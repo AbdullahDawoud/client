38b21ef1f597b796b10cb428e179d8b8297196ab
AbdullahDawoud <abdullahdawoud@gmail.com>
2022-08-11 15:32:34 +0300

    prettier

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import RootLayout from "./src/roots-layout";
+import "./src/assets/styles/global.scss";
 
 export const wrapRootElement = ({ element }) => (
   <RootLayout>{element}</RootLayout>
--- src/roots-layout.tsx
+++ src/roots-layout.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import Layout from "./components/layout";
-import { QueryClient, QueryClientProvider } from "react-query";
-import "normalize.css";
+import React from 'react';
+import Layout from './components/layout';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import 'normalize.css';
 
 export const client = new QueryClient();
 
 type Props = {
-  children: Node;
+    children: Node;
 };
 const RootLayout = ({ children }: Props) => {
-  return (
-    <QueryClientProvider client={client}>
-      <Layout>{children}</Layout>
-    </QueryClientProvider>
-  );
+    return (
+        <QueryClientProvider client={client}>
+            <Layout>{children}</Layout>
+        </QueryClientProvider>
+    );
 };
 
 export default RootLayout;
